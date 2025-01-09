f8e67a9e8eb77c74a5e6ee194d8b8e008625b2c3
Zeel Patel <zeelpatel6878@gmail.com>
2025-01-09 11:59:10 +0530

    add a markdown editior

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {Button} from '@radix-ui/themes'
 import Link from 'next/link'
 
+
 const IssuePage = () => {
   return (
     <div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,17 @@
 import "@radix-ui/themes/styles.css";
+import './theme.config.css';
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Theme } from '@radix-ui/themes'
+import { Theme} from '@radix-ui/themes'
 import Navbar from "./Navbar";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+	subsets: ["latin"],
+	display: "swap",
+	variable: "--font-inter",
+});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,15 +34,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.variable}>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Theme>
+        <Theme appearance="light" accentColor="violet">
           <Navbar />
           <main className="p-5">
             {children}
           </main>
+          {/* <ThemePanel/> */}
         </Theme>
       </body>
     </html>
